cb044296944fbf35489302c48ac07a8e773e4ec0
Injamamul Haque Sonet <ihsonnet@gmail.com>
2021-08-30 22:58:35 +0600

    assetsImage + set icon and test done

--- i_am_rich_app/lib/main.dart
+++ i_am_rich_app/lib/main.dart
@@ -4,26 +4,16 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("I Am Rich"),
+        title: Center(child: Text("I am Rich"),),
         backgroundColor: Colors.blueGrey[800],
       ),
       body: Center(
-          child: Column(
-        children: [
-          Center(
-            child: Text(
-              'This is Body',
-              style: TextStyle(fontSize: 15),
-            ),
+          child: Image(
+            image: AssetImage(
+                'images/diamond.png'), width: 300,
           ),
-          Image(
-            image: NetworkImage(
-                'https://flutter.github.io/assets-for-api-docs/assets/widgets/row.png'), width: 200,
-          ),
-        ],
       ),
-      ),
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blueGrey,
     ),
   ));
 }
